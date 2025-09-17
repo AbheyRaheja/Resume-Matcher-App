a8043f7497adb7bd21dc5701eaff3982dfd493f4
Abhey <abheyraheja07479@gmail.com>
2025-09-17 18:32:14 +0530

    Adding the actual fastapi deployed weblink to the frontend

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://resume-matcher-app-e1k0.onrender.com"
 UPLOAD_ENDPOINT = f"{BASE_URL}/resume/upload"
 
 st.set_page_config(page_title="Resume Job Matcher", layout="wide")
